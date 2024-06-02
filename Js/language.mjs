a67f000fc8661305c247996a8e58ea8d01d7d6c6
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-06-02 22:11:07 +0200

    now?

--- Js/language.mjs
+++ Js/language.mjs
@@ -41,13 +41,8 @@ export function redirectToLanguage(languageCode) {
   // Split the path into segments
   const pathSegments = path.split("/");
 
-  // Find the position of the language prefix, starting from the repository name
-  let languageIndex = pathSegments.indexOf("testing-java") + 1;
-
-  // If the language prefix is not immediately after the repository name, search for it in subdirectories
-  if (languageIndex === -1) {
-    languageIndex = pathSegments.findIndex((segment) => segment === "no" || segment === "mg");
-  }
+  // Find the position of the language prefix
+  let languageIndex = pathSegments.indexOf(languageCode);
 
   // Handle the language prefix based on the selected language code
   if (languageCode === "default") {
@@ -58,14 +53,27 @@ export function redirectToLanguage(languageCode) {
   } else {
     // Insert or update the language prefix
     if (languageIndex === -1) {
-      pathSegments.splice(1, 0, languageCode);
-    } else if (pathSegments[languageIndex] !== languageCode) {
-      pathSegments[languageIndex] = languageCode;
+      // If the language prefix doesn't exist, insert it after the repository name
+      const repoIndex = pathSegments.indexOf("testing-java");
+      if (repoIndex !== -1) {
+        pathSegments.splice(repoIndex + 1, 0, languageCode);
+      } else {
+        // If the repository name doesn't exist, prepend the language prefix
+        pathSegments.unshift(languageCode);
+      }
+    } else if (pathSegments[languageIndex - 1] !== "testing-java") {
+      // If the language prefix is not immediately after the repository name, update it
+      pathSegments[languageIndex - 1] = languageCode;
     }
   }
 
+  // Join the path segments back together
   path = pathSegments.join("/");
+
+  // Update the pathname of the URL
   url.pathname = "/" + path;
+
+  // Navigate to the new URL
   window.location.href = url.href;
 }
 
@@ -74,51 +82,3 @@ export function redirectToSelectedLanguage() {
   const selectedLanguage = languageSelect.value;
   redirectToLanguage(selectedLanguage);
 }
-
-// export function redirectToLanguage(languageCode) {
-//   const currentUrl = window.location.href;
-//   const url = new URL(currentUrl);
-//   const pathSegments = url.pathname.split("/");
-
-//   const supportedLanguages = ["no", "default"]; // Legg til flere språk her
-
-//   if (languageCode === "default") {
-//     if (supportedLanguages.includes(pathSegments[1])) {
-//       pathSegments.splice(1, 1);
-//     }
-//   } else {
-//     if (supportedLanguages.includes(pathSegments[1])) {
-//       pathSegments[1] = languageCode;
-//     } else if (pathSegments[1] !== languageCode) {
-//       pathSegments.splice(1, 0, languageCode);
-//     }
-//   }
-
-//   const newPathname = pathSegments.join("/");
-//   url.pathname = newPathname;
-
-//   window.location.href = url.href;
-// }
-
-// export function setLanguage(languageCode) {
-//   localStorage.setItem("preferredLanguage", languageCode);
-//   redirectToLanguage(languageCode);
-// }
-
-// document.addEventListener("DOMContentLoaded", (event) => {
-//   const preferredLanguage = localStorage.getItem("preferredLanguage");
-//   if (preferredLanguage && preferredLanguage !== "default") {
-//     const currentUrl = window.location.href;
-//     const url = new URL(currentUrl);
-//     const pathSegments = url.pathname.split("/");
-
-//     if (
-//       !(
-//         pathSegments[1] === preferredLanguage ||
-//         (preferredLanguage === "default" && !supportedLanguages.includes(pathSegments[1]))
-//       )
-//     ) {
-//       redirectToLanguage(preferredLanguage);
-//     }
-//   }
-// });
